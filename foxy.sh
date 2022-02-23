fb05f3844b624dcee5184174e7a2602c8edcf2be
samuk <sam@bristolwireless.net>
2022-02-23 00:52:37 +0000

    Update and rename dashing.sh to foxy.sh
    Update for foxy

--- foxy.sh
+++ foxy.sh
@@ -17,14 +17,14 @@ sudo apt-get update && sudo apt-get install -y curl gnupg2 lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-# install ros2 dashing
+# install ros2 foxy
 sudo apt-get update
-sudo apt-get install -y ros-dashing-desktop python3-colcon-common-extensions python-rosdep
+sudo apt-get install -y ros-foxy-desktop python3-colcon-common-extensions python-rosdep
 sudo rosdep init
 rosdep update
 
-# source ros2 dashing
-source /opt/ros/dashing/setup.bash
+# source ros2 foxy
+source /opt/ros/foxy/setup.bash
 echo  'source /opt/ros/dashing/setup.bash' >> ~/.bashrc 
 
 # record script end time
